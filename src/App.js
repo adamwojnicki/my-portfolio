a5140286f630c249fd7555af73bd489a496c01f0
Adam Wojnicki <wojnicki.ada1@gmial.com>
2020-07-30 20:38:12 +0200

    Layout planning and scss integration

--- src/App.js
+++ src/App.js
@@ -1,5 +1,24 @@
 import React from "react";
+import "./App.scss";
 
 export default () => {
-  return <div className="App"></div>;
+  return (
+    <div className="container">
+      <section className="content-section" id="personal">
+        personal
+      </section>
+      <section className="content-section" id="skills">
+        skills
+      </section>
+      <section className="content-section" id="experiences">
+        experiences
+      </section>
+      <section className="content-section" id="projects">
+        projects
+      </section>
+      <section className="content-section" id="hobbies">
+        hobbies
+      </section>
+    </div>
+  );
 };
