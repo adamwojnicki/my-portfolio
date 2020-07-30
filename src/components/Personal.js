cba868e99017742744c9f691b94f15a65101163d
Adam Wojnicki <wojnicki.ada1@gmial.com>
2020-07-30 20:54:25 +0200

    Basic layout and components creation

---
+++ src/components/Personal.js
@@ -0,0 +1,10 @@
+import React from "react";
+import styles from "./Personal.module.scss";
+
+export default () => {
+  return (
+    <section className={`content-section ${styles.personal}`} id="personal">
+      personal
+    </section>
+  );
+};
