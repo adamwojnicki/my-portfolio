dbb3cdca5490cdcf8354e312aa0bb32004f15f9f
Adam Wojnicki <wojnicki.ada1@gmial.com>
2020-08-25 15:35:29 +0200

    Working on Project component reusability

--- src/components/Projects/Project.js
+++ src/components/Projects/Project.js
@@ -1,25 +1,21 @@
 import React from "react";
-import edieImg from "../../images/projects/edie-page.png";
 import styles from "./Project.module.scss";
 
-export default ({ title }) => {
+export default ({ img, tags, title, desc, live, code }) => {
+  const renderTags = tags.map((tag) => (
+    <li className={styles.tags__item}>{tag}</li>
+  ));
   return (
     <li className={styles.item}>
       <figure className={styles.item__img}>
-        <img src={edieImg} alt="Screenshot of a project" />
+        <img src={img} alt="Screenshot of a project" />
       </figure>
-      <ul className={styles.tags}>
-        <li className={styles.tags__item}>#frontend</li>
-        <li className={styles.tags__item}>#responsive</li>
-      </ul>
+      <ul className={styles.tags}>{renderTags}</ul>
       <h3 className={styles.item__headline}>{title}</h3>
-      <p className={styles.item__description}>
-        Page made with raw HTML and CSS. No libraries, frameworks, preprocessors
-        were used in this project
-      </p>
+      <p className={styles.item__description}>{desc}</p>
       <a
         className={styles.item__ctaPrimary}
-        href="https://keen-lichterman-5dd3d9.netlify.app/"
+        href={live}
         target="_blank"
         rel="noreferrer noopener"
       >
@@ -28,7 +24,7 @@ export default ({ title }) => {
 
       <a
         className={styles.item__cta}
-        href="https://github.com/adamwojnicki/eddie-page"
+        href={code}
         target="_blank"
         rel="noreferrer noopener"
       >
--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -1,14 +1,37 @@
 import React from "react";
 import Project from "./Project";
+import edieImg from "../../images/projects/edie-page.png";
+
+const projects = [
+  {
+    id: 1,
+    img: edieImg,
+    tags: ["#landing", "#responsive"],
+    title: "Edie landing page",
+    desc:
+      "Page made with raw HTML and CSS. No libraries, frameworks, preprocessors were used in this project",
+    liveUrl: "https://keen-lichterman-5dd3d9.netlify.app/",
+    codeUrl: "https://github.com/adamwojnicki/eddie-page/",
+  },
+];
+
+const projectElements = projects.map((project) => (
+  <Project
+    key={project.id}
+    tags={project.tags}
+    img={project.img}
+    title={project.title}
+    desc={project.desc}
+    live={project.liveUrl}
+    code={project.codeUrl}
+  />
+));
 
 export default () => {
   return (
     <section className="content-section" id="projects">
       <h2 className="content-section__headline">Projects</h2>
-      <ul>
-        <Project title="Edie landing page" />
-        <Project title="ababababa" />
-      </ul>
+      <ul>{projectElements}</ul>
     </section>
   );
 };
